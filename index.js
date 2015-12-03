8a0b653185a002eedf0406f995e654bccfecaf04
Héctor Gómez <hector@iMac-Hector.local>
2015-12-03 21:36:32 +0100

    Added some documentation to the JavaScript code

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ jQuery('#form-submit-button').on('click', function() {
     submitForm();
 });
 
+/**
+ * Called after the form was successfully submitted
+ */
+function onSubmitSuccess() {
+    alert("Thank you for your message,\nI will get back to you as soon as possible!");
+    document.contact.reset();
+}
+
+/**
+ * Called when the form failed to load
+ *
+ * @param promise Promise object that contains the server response
+ */
+function onSubmitFailure(promise) {
+    var response = JSON.parse(promise.responseText);
+    alert(response['msg']);
+}
+
+/**
+ * Submits the contents of the contact form
+ */
 function submitForm() {
     var data = {
         'name': document.contact.name.value,
@@ -14,14 +35,4 @@ function submitForm() {
         .post('send-mail.php', data)
         .done(onSubmitSuccess)
         .fail(onSubmitFailure);
-}
-
-function onSubmitSuccess() {
-    alert("Thank you for your message,\nI will get back to you as soon as possible!");
-    document.contact.reset();
-}
-
-function onSubmitFailure(promise) {
-    var response = JSON.parse(promise.responseText);
-    alert(response['msg']);
 }
